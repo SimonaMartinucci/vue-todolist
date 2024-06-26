7f71d98147ae19a881973f78a3bd31675f7bf4a1
SimonaMartinucci <simo.martinucci3@gmail.com>
2024-06-26 16:09:54 +0200

    feat(new bonus): add error message if input not correct + add message 'no more tasks' if user delete all the tasks

--- js/myscript.js
+++ js/myscript.js
@@ -11,24 +11,38 @@ createApp({
                 {text: "Fare spesa", done: false}
             ],
             newTask: '',
+            inputError: false
         }
     },
     methods: {
         removeTask(indice) {
+            // tolgo solo task specifica
             this.taskList.splice(indice, 1);
         },
 
         addTask() {
-            this.taskList.unshift({text: this.newTask, done: false}),
+            // controllo che la task inserita dall'utente abbia i requisiti corretti
+            if(this.newTask.length >= 5 && this.newTask !== '') {
+                // se li ha aggiungo nuovo obj alla lista
+                this.taskList.unshift({text: this.newTask, done: false});
+                // non faccio uscire messaggio di errore
+                this.inputError = false;
+            } else {
+                // se non li ha faccio uscire messaggio di errore
+                this.inputError = true;
+            }
+            // svuoto l'input ogni volta che l'utente clicca
             this.newTask = '';
         },
 
         statusTask(indice) {
+            // cambio valore di 'done' sulla base dei click
             if(this.taskList[indice].done === true) {
                 this.taskList[indice].done = false;
             }else {
                 this.taskList[indice].done = true;
             }
-        }
+        },
+
     }
 }).mount('#app');
