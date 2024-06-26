f7a24bd519ea256e4e09bb44bb5b6a1a2a783e06
SimonaMartinucci <simo.martinucci3@gmail.com>
2024-06-26 14:14:30 +0200

    feat: add array + li + class 'line-through'

--- js/myscript.js
+++ js/myscript.js
@@ -3,7 +3,13 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-
+            taskList: [
+                {text: "Fare lavatrice", done: true},
+                {text: "Pulire bagno", done: true},
+                {text: "Sistemare panni stesi", done: false},
+                {text: "Lavare pavimento", done: true},
+                {text: "Fare spesa", done: false}
+            ],
         }
     },
     methods: {
